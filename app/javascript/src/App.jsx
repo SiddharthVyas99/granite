e13a5122b1e44898f34c10cd49a5ec8ba83c83dd
Siddharth <siddharth.vyas9299@gmail.com>
2021-05-19 22:07:12 +0530

    Added ability to add new users

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CreateTask from "components/Tasks/CreateTask";
 
 import Dashboard from "components/Dashboard";
 import ShowTask from "components/Tasks/ShowTask";
 import EditTask from "components/Tasks/EditTask";
+import Signup from "components/Authentication/Signup";
+import PageLoader from "components/PageLoader";
 
 import { initializeLogger } from "common/logger";
 import { ToastContainer } from "react-toastify";
@@ -11,12 +13,23 @@ import { registerIntercepts } from "apis/axios";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 const App = () => {
+  // const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     /*eslint no-undef: "off"*/
     initializeLogger();
     registerIntercepts();
+    // setAuthHeaders(setLoading);
     logger.info("Log from js-logger");
   }, []);
+
+  // if (loading) {
+  //   return (
+  //     <div className="h-screen">
+  //       <PageLoader />
+  //     </div>
+  //   );
+  // }
 
   return (
     <Router>
@@ -27,6 +40,7 @@ const App = () => {
         <Route exact path="/tasks/:slug/show" component={ShowTask} />
         <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/signup" component={Signup} />
       </Switch>
     </Router>
   );
