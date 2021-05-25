2eb476d43ed4dff2d21ab59f2eca454b17256f1d
Siddharth <siddharth.vyas9299@gmail.com>
2021-05-26 03:02:41 +0530

    Unit tests on task model updated

--- db/migrate/20210515090701_make_title_not_nullable.rb
+++ db/migrate/20210515090701_make_title_not_nullable.rb
@@ -1,14 +1,14 @@
 class MakeTitleNotNullable < ActiveRecord::Migration[6.0]
-  # def up
-  #   add_column :tasks, :title, :text
-  #   change_column_null :tasks, :title, false
-  # end
-
-  def change
+  def up
+    add_column :tasks, :title, :text
     change_column_null :tasks, :title, false
   end
 
-  # def down
-  #   remove_column :tasks, :title
+  # def change
+  #   change_column_null :tasks, :title, false
   # end
+
+  def down
+    remove_column :tasks, :title
+  end
 end
